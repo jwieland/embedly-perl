6ed7ce64a2cb6c3bca2218a14973b747c78a6b35
Jason Wieland <jwieland@gmail.com>
2012-03-04 20:43:24 -0800

    near release..  added http error response and finished documentation

--- lib/WWW/Embedly.pm
+++ lib/WWW/Embedly.pm
@@ -1,11 +1,22 @@
 package WWW::Embedly;
 
 use Mouse;
+use Mouse::Util::TypeConstraints;
 use MouseX::Types::URI;
+
+use LWP::UserAgent;
+use JSON::XS;
+#use URI;
+use URI::Escape;
+
+use Ouch qw(:traditional);
+use Regexp::Common qw /URI/;
+
+#use Data::Dumper::Simple;
 
 =head1 NAME
 
-WWW::Embedly - Use Embed.ly API to pull info about asset url
+WWW::Embedly - Simple way to intergrate into Embed.ly API
 
 =head1 VERSION
 
@@ -18,40 +29,431 @@ our $VERSION = '0.01';
 =head1 SYNOPSIS
 
     use WWW::Embedly;
+    use Ouch qw(:traditional);
 
-    my $embedly = WWW::Embedly->new({ api_key => '21212112121'});
+    my $embedly = WWW::Embedly->new({ api_key => 'get_your_key_at_embed.ly'});
+
+    my $oembed_ref;
+    my $e = try {
+      $oembed_ref = $embedly->oembed('http://youtu.be/I8CSt7a7gWY');
+    };
+
+    if ( catch 500, $e) {
+       #Server is down
+       return;
+    }
+    if ( catch 401, $e) {
+       #Your API key has used all its credits
+       return;
+    }
+    elsif ( catch_all, $e) {
+       #I hate the individual exception catching, lets get this over with it.
+       return;
+    }
+
+    #made it here, everything good.
+    my $embed_html = $oembed_ref->{html}
 
 =cut
 
+=head1 DESCRIPTION
 
-=head1 Attributes
+The C<WWW::Embedly> is a class implementing for quering the Embed.ly web service.  Prior to using this module you should go to L<http://embed.ly> and sign up for an api_key.
+
+C<WWW::Embedly> exposes three methods: oembed, preview, objectify.  Each method has addtional bits of metadata about the request URL.  Refer to L<http://embed.ly> to leanr more about the data that is returned.
+
+Exception handling is used to expose failures. The Ouch module (:traditional) is used to handle try/catch blocks.  See the Excpetion block below for all the possible catches.
+
+C<WWW::Embedly> uses Mouse (lighter version of Moose) to handle its object management.
+
+=cut
+
+=head1 CONSTRUCTOR
+
+You must pass the api_key into the contructor:
+
+    my $embedly = WWW::Embedly->new({ api_key => 'get_your_key_at_embed.ly'});
+
+C<WWW::Embedly> uses LWP::UserAgent to handle its web requests.  You have the ability to pass in your own LWP object in case you have special requirements, like a proxy server.
+
+    my $ua = LWP::UserAgent->new();
+    $ua->proxy('http', 'http://proxy.sn.no:8001/');
+
+    my $embedly = WWW::Embedly->new({ api_key => 'get_your_key_at_embed.ly',
+                                      ua => $ua
+                                    });
+
+=head2 Optional Params
+
+C<WWW::Embedly> supports all optional parameters at the time of this writting L<http://embed.ly/docs/endpoints/arguments>.  Refere to the embedly documentation for the complete description.  In most cases you only need to worry about the maxwidth param.  It is highly recommeneded to specify the maxwidth other parameters are not needed in most cases.
+
+=head3 maxwidth
+
+This is the maximum width of the embed in pixels. maxwidth is used for scaling down embeds so they fit into a certain width.  If the container for an embed is 500px you should pass maxwidth=500 in the query parameters. 
+
+=head3 maxheight
+
+This is the maximum height of the embed in pixels.
+
+=head3 width
+
+Will scale embeds type rich and video to the exact width that a developer specifies in pixels.
+
+=head3 format (default: json)
+
+The response format – Accepted values: (xml, json)
+
+=head3 callback
+
+Returns a (jsonp) response format. The callback is the name of the javascript function to execute.
+
+=head3 wmode 
+
+Will append the wmode value to the flash object. Possible values include window, opaque and transparent.
+
+=head3 allowscripts (default: false)
+
+By default Embedly does not return script embeds for jsonp requests. They just don’t work and cause lots of issues. In some cases, you may need the script tag for saving and displaying later.  Accepted values: (true, false)
+
+=head3 nostyle (default: false)
+
+There are a number of embeds that Embedly has created including Amazon.com, Foursquare, and Formspring. These all have <style> elements and inline styles associated with them that make the embeds look good. Accepted values: (true, false)
+
+=head3 autoplay (default: false)
+
+This will tell the video/rich media to automatically play when the media is loaded. Accepted values: (true, false)
+
+=head3 videosrc (default: false)
+
+Either true Embedly will use the video_src meta or Open Graph tag to create a video object to embed. Accepted values: (true, false)
+
+=head3 words
+
+The words parameter has a default value of 50 and works by trying to split the description at the closest sentence to that word count.
+
+=head3 chars
+
+chars is much simpler than words. Embedly will blindly truncate a description to the number of characters you specify adding ... at the end when needed.
+
+=cut
+
+=head1 EXCEPTIONS
 
 =cut
 
 has 'api_key' => (
-		  is  => 'rw',
+		  is  => 'ro',
 		  isa => 'Str',
 		  required => 1,
 );
 
-has 'uri' => (
-    is     => 'rw',
-    isa    => 'URI',
-    coerce => 1,
-);
-
-has 'oembed_base' => (
+has 'oembed_base_uri' => (
 		       is => 'ro',
 		       isa => 'Str',
-		       default => 'http://api.embed.ly/1/oembed?'
+		       default => 'http://api.embed.ly/1/oembed'
 );
 
-=head1 SUBROUTINES/METHODS
+has 'preview_base_uri' => (
+		       is => 'ro',
+		       isa => 'Str',
+		       default => 'http://api.embed.ly/1/preview'
+);
 
-=head2 
+has 'objectify_base_uri' => (
+		       is => 'ro',
+		       isa => 'Str',
+		       default => 'http://api.embed.ly/1/objectify'
+);
+
+has 'ua' => (
+	     is => 'ro',
+#	     isa => duck_type ['get'],
+	     isa => 'LWP::UserAgent',
+	     required => 1,
+	     default => sub { LWP::UserAgent->new;  },
+);
+
+has 'json' => (
+	     is => 'ro',
+	     isa => 'JSON::XS',
+	     required => 1,
+	     default => sub { JSON::XS->new->utf8; },
+);
+
+has 'maxwidth' => (
+		   is  => 'rw',
+		   isa => 'Int',
+		   required => 0,
+		   predicate => 'has_maxwidth'
+		  );
+
+has 'maxheight' => (
+		    is  => 'rw',
+		    isa => 'Int',
+		    required => 0,
+		    predicate => 'has_maxheight'
+		   );
+
+has 'width' => (
+		is  => 'rw',
+		isa => 'Int',
+		required => 0,
+		predicate => 'has_width'
+	       );
+
+has 'format' => (
+		   is  => 'rw',
+		   isa => 'Str',
+		   required => 0,
+		   predicate => 'has_format'
+		  );
+
+has 'callback' => (
+		   is  => 'rw',
+		   isa => 'Str',
+		   required => 0,
+		   predicate => 'has_callback'
+		  );
+
+has 'wmode' => (
+		is  => 'rw',
+		isa => 'Str',
+		required => 0,
+		predicate => 'has_wmode'
+	       );
+
+has 'allowscripts' => (
+		       is  => 'rw',
+		       isa => 'Str',
+		       required => 0,
+		       predicate => 'has_allowscripts'
+		      );
+
+
+has 'nostyle' => (
+		  is  => 'rw',
+		  isa => 'Str',
+		  required => 0,
+		  predicate => 'has_nostyle'
+	       );
+
+has 'autoplay' => (
+		   is  => 'rw',
+		   isa => 'Str',
+		   required => 0,
+		   predicate => 'has_autoplay'
+		  );
+
+has 'videosrc' => (
+		   is  => 'rw',
+		   isa => 'Str',
+		   required => 0,
+		   predicate => 'has_videosrc'
+	       );
+
+
+has 'words' => (
+		is  => 'rw',
+		isa => 'Int',
+		required => 0,
+		predicate => 'has_words'
+	       );
+
+
+has 'chars' => (
+		is  => 'rw',
+		isa => 'Int',
+		required => 0,
+		predicate => 'has_chars'
+	       );
+
+
+
+#optional parms
+
+=head1 METHODS
+
+=head2 oembed;
+
+=head2 preview;
+
+=head2 objectify;
+
+Embed.ly provide three different methods: oembed, preview, objectify depending on the amount of information/access you need each take the same parameters.  However different data is returned depedning on which method used.
+
+There are three ways to call each method
+
+=head3 Single URL
+
+Fetch metadata about a single URL - call method with full url as a string
+
+    $oembed_ref = $embedly->oembed('http://youtu.be/I8CSt7a7gWY');
+
+=head3 Multiple URLs
+
+Fetch metadata about multiple URLs - call method with array ref of urls
+
+   my @urls = qw(http://yfrog.com/ng41306327j http://twitter.com/embedly/status/29481593334 http://blog.embed.ly/31814817 http://soundcloud.com/mrenti/merenti-la-karambaa);
+   $oembed_ref = $embedly->oembed(\@urls);
+
+=head3 Extra Information
+
+Fetch metadata about URL(s) and include additional query arguments L<http://embed.ly/docs/endpoints/arguments> - call methos with with hash ref of attributes
+
+   my $query_ref = {
+
+
+Can throw an exception (ouch) so wrap in an eval or use Ouch module and refer to its syntax
 
 =cut
 
+sub oembed {
+  my ($self, $embed_url) = @_;
+
+  my $res = $self->_request( { embed_url => $embed_url,
+			       base_uri  => $self->oembed_base_uri
+			     } );
+
+  return ($res);
+}
+
+
+sub preview {
+  my ($self, $embed_url) = @_;
+
+  my $res = $self->_request( { embed_url => $embed_url,
+			       base_uri  => $self->preview_base_uri
+			     } );
+
+  return ($res);
+}
+
+sub objectify {
+  my ($self, $embed_url) = @_;
+
+  my $res = $self->_request( { embed_url => $embed_url,
+			       base_uri  => $self->objectify_base_uri
+			     } );
+
+  return ($res);
+}
+
+sub _request {
+  my ($self, $in) = @_;
+
+  my $embed_url = $in->{embed_url};
+  my $base_uri  = $in->{base_uri};
+
+  my %params = ( key => $self->api_key );
+
+  if (ref($embed_url) eq 'ARRAY') {
+    my $cnt = int(@{$embed_url});
+    if ($cnt > 20) {
+      throw 400, 'Cannot pass more than 20 urls in a single request';
+    }
+
+    my @escaped_urls;
+    foreach my $e_url (@{$embed_url}) {
+      unless ( $RE{URI}{HTTP}->matches($e_url) ) {
+	throw 400, 'Lookup URL does not look like a properly formatted url: ' . $e_url;
+      }
+      push @escaped_urls, uri_escape($e_url) ;
+    }
+    $params{urls} = join (',', @escaped_urls);
+  }
+  else {
+    #quick check to see if we have a url
+    unless ( $RE{URI}{HTTP}->matches($embed_url) ) {
+      throw 400, 'Lookup URL does not look like a properly formatted url: ' . $embed_url;
+    }
+    $params{url} = uri_escape($embed_url);
+  }
+
+#  can't use query_form because embedly can't handle full application/x-www-form-urlencoded for multi urls (the comma between urls needs to stay a comma and not escaped to %2C) boo
+#  my $uri = URI->new($base_uri);
+#  $uri->query_form($params);
+
+  my $url = $base_uri . '?';
+  foreach my $param (keys %params) {
+    $url .= $param .'='. $params{$param} . '&';
+  }
+
+  if ( $self->has_maxwidth ) {
+    $url .= 'maxwidth='. $self->maxwidth . '&';
+  }
+
+  if ( $self->has_maxheight ) {
+    $url .= 'maxheigth='. $self->maxheight . '&';
+  }
+
+  if ( $self->has_width ) {
+    $url .= 'width='. $self->width . '&';
+  }
+
+  if ( $self->has_format ) {
+    $url .= 'format='. $self->format . '&';
+  }
+
+  if ( $self->has_callback ) {
+    $url .= 'callback='. $self->callback . '&';
+  }
+
+  if ( $self->has_wmode ) {
+    $url .= 'wmode='. $self->wmode . '&';
+  }
+
+  if ( $self->has_allowscripts ) {
+    $url .= 'allowscripts=true&';
+  }
+
+  if ( $self->has_nostyle ) {
+    $url .= 'nostyle=true&';
+  }
+
+  if ( $self->has_autoplay ) {
+    $url .= 'autoplay=true&';
+  }
+
+  if ( $self->has_videosrc ) {
+    $url .= 'videosrc=true&';
+  }
+
+  if ( $self->has_words ) {
+    $url .= 'words='. $self->words . '&';
+  }
+
+  if ( $self->has_chars ) {
+    $url .= 'chars='. $self->chars . '&';
+  }
+
+  #take off the trailing '&'
+  chop ($url);
+
+#  warn ($url);
+
+  my $res = $self->ua->get($url);
+
+  if ($res->is_success) {
+    my $data_ref;
+    eval {
+      $data_ref = $self->json->decode($res->decoded_content);
+    };
+    if ($@) {
+      #Should be a different code than 500
+      throw 500, 'Could not parse JSON response', $@;
+    }
+    return $data_ref;
+  }
+  else {
+    ## figure out all the different failures form the API
+    if ($res->code) {
+      throw $res->code, $res->status_line;
+    }
+    else {
+      throw 500, 'HTTP Request to embed.ly failed', $res->status_line;
+    }
+  }
+}
 
 =head1 AUTHOR
 
@@ -101,13 +503,13 @@ L<http://search.cpan.org/dist/WWW-Embedly/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2012 Jason Wieland.
+Copyright 2012 Jason Wieland and 12engines LLC
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
 by the Free Software Foundation; or the Artistic License.
 
-See http://dev.perl.org/licenses/ for more information.
+See L<http://dev.perl.org/licenses/> for more information.
 
 
 =cut
---
+++ t/01-basic-usage.t
@@ -0,0 +1,63 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use Test::More;
+
+use Test::Mock::LWP::Dispatch;
+use LWP::UserAgent;
+use HTTP::Response;
+use HTTP::Headers;
+
+use Ouch qw(:traditional);
+
+#Setup Expected Responses
+my $oembed_res = '{"provider_url": "http://www.youtube.com/", "description": "CGRtrailers, from Classic Game Room\u00ae, presents the \"Rhino\" trailer for THE AMAZING SPIDER-MAN from Beenox, Activision and Marvel Entertainment. This game featuring everyones favorite webslinging superhero is inspired/based upon the upcoming summer movie. Enjoy!", "title": "THE AMAZING SPIDER-MAN Rhino Trailer", "url": "http://www.youtube.com/watch?v=I8CSt7a7gWY", "html": "<iframe width=\"640\" height=\"360\" src=\"http://www.youtube.com/embed/I8CSt7a7gWY?fs=1&feature=oembed\" frameborder=\"0\" allowfullscreen></iframe>", "author_name": "CGRtrailers", "height": 360, "width": 640, "thumbnail_url": "http://i2.ytimg.com/vi/I8CSt7a7gWY/hqdefault.jpg", "thumbnail_width": 480, "version": "1.0", "provider_name": "YouTube", "type": "video", "thumbnail_height": 360, "author_url": "http://www.youtube.com/user/CGRtrailers"}';
+
+BEGIN { use_ok( 'WWW::Embedly') ; }
+use WWW::Embedly;
+
+my $embedly;
+my $ua = LWP::UserAgent->new();
+my $headers = HTTP::Headers->new(
+				 'Content-Type'   => 'application/json',
+				 'Server' => 'TornadoServer/2.0'
+				);
+$headers->date;
+
+#oembed
+$mock_ua->map(
+	      'http://api.embed.ly/1/oembed?url=http%3A%2F%2Fyoutu.be%2FI8CSt7a7gWY&key=test&maxwidth=500',
+	      sub {
+		my $req = shift;
+		my $uri = $req->uri;
+		my $content = $oembed_res;
+		$headers->content_length(length($content));
+		my $res = HTTP::Response->new(200, 'OK', $headers, $content);
+		return $res;
+	      },
+);
+
+
+#pass in mock user agent..  IoC testing
+$embedly = WWW::Embedly->new({ api_key => 'test',
+			       maxwidth => 500,
+			       ua => $ua
+			     });
+
+isa_ok($embedly, 'WWW::Embedly');
+isa_ok($embedly->ua, 'LWP::UserAgent');
+
+my $url = 'http://youtu.be/I8CSt7a7gWY';
+my $e = try {
+  my $oembed_ref = $embedly->oembed($url);
+  is ($oembed_ref->{provider_url}, 'http://www.youtube.com/', 'Oembed response' );
+};
+
+if (catch_all $e) {
+  fail 'Failed oembed request' ;
+}
+
+
+done_testing();
